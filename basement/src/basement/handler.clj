57ff57749b21ce76d85cab1ffeb4f1aac410640a
Simon Koelsch <echox@echox.de>
2015-03-08 16:20:24 +0100

    removed example code

--- basement/src/basement/handler.clj
+++ basement/src/basement/handler.clj
@@ -3,7 +3,6 @@
             [compojure.route :as route]
             [compojure.handler :as handler]
 
-            [basement.users]
             [basement.api.users]
 
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
@@ -13,26 +12,16 @@
 
 (use 'basement.api.users)
 
-(defn testout [r]
-	"bar 2"
-)
-
 (defn decorate [data]
   (req/header (response data)
     "Content-Type" "application/org.raumzeitlabor.benutzerdb-v1+json; charset=utf-8"
   )
 )
 
-
-
 (defroutes app-routes
   (GET "/" [] "Hello World")
-	(GET "/test" [] testout)
-  (GET "/users" [] (decorate (lookup-user "foo")))
   (GET "/users/:id" [id] (decorate (lookup-user id)))
 	)
-
-
 
 (def basement
   (-> app-routes
--- basement/src/basement/users.clj
+++
@@ -1,8 +0,0 @@
-(ns basement.users)
-
-(defn user [firstname lastname]
-  {:firstname firstname :lastname lastname})
-
-
-(defn userlist []
-  (user "test" "user"))
