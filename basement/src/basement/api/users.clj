1d7b002009e8729a22e77ba9242883af74e86c46
Simon Koelsch <echox@echox.de>
2015-03-08 01:19:31 +0100

    lookup a user

--- basement/src/basement/api/users.clj
+++ basement/src/basement/api/users.clj
@@ -7,6 +7,15 @@
    :email email
    :contribution-receipt contribution-receipt})
 
+(defn link [rel type href]
+  {:rel rel
+   :type type
+   :href href})
+
+(defn add-self [user]
+  (assoc user :link (link "self"
+                          "application/org.raumzeitlabor.benutzerdb-v1+json"
+                          (str "users/" (user :nick)))))
+
 (defn lookup-user [userid]
-    (user "foobar" "Martin" "Mustermann" "foo@example.org" false)
-  )
+    (add-self (user userid "Martin" "Mustermann" "foo@example.org" false)))
--- basement/src/basement/handler.clj
+++ basement/src/basement/handler.clj
@@ -4,12 +4,14 @@
             [compojure.handler :as handler]
 
             [basement.users]
+            [basement.api.users]
 
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
             [ring.util.response :refer [resource-response response]]
             [ring.util.response :as req]
             [ring.middleware.json :as middleware]))
 
+(use 'basement.api.users)
 
 (defn testout [r]
 	"bar 2"
@@ -18,12 +20,13 @@
 (defroutes app-routes
   (GET "/" [] "Hello World")
 	(GET "/test" [] testout)
-  (GET "/users" [] (decorate basement.users/userlist))
+  (GET "/users" [] (decorate (lookup-user "foo")))
+  (GET "/users/:id" [id] (decorate (lookup-user id)))
 	)
 
 
-(defn decorate [func]
-  (req/header (response (func))
+(defn decorate [data]
+  (req/header (response data)
     "Content-Type" "application/org.raumzeitlabor.benutzerdb-v1+json; charset=utf-8"
   )
 )
