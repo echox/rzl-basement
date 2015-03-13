7a2aeb8bc8a2c006ef770d806410a178609c474f
Simon Koelsch <echox@echox.de>
2015-03-13 21:57:47 +0100

    added vcard support for user, added multi-content negoiation

--- basement/src/basement/handler.clj
+++ basement/src/basement/handler.clj
@@ -17,9 +17,37 @@
 
 (defn decorate [data]
   (req/header (response data)
-    "Content-Type" "application/org.raumzeitlabor.benutzerdb-v1+json; charset=utf-8"
-  )
-)
+    "Content-Type" "application/org.raumzeitlabor.benutzerdb-v1+json; charset=utf-8"))
+
+
+(defn decorate-vcard [data]
+  (req/header (response data)
+    "Content-Type" "text/vcard; charset=utf-8"))
+
+(defn transform-vcard [user]
+  (str "BEGIN:VCARD\n"
+       "VERSION:4.0\n"
+       "N:"(:lastname user)";"(:firstname user)";;;\n"
+       "EMAIL:"(:email user)"\n"
+       ;; add REV
+       "END:VCARD"))
+
+(defn negotiator [handlers]
+  (fn [request]
+    (let [header (clojure.string/split (get (:headers request) "accept") #",")
+          selected (first (drop-while (fn [item]
+                   (not(contains? handlers item)))
+                 header))]
+
+    (if (contains? handlers selected)
+      (get handlers selected)
+      (if (contains? handlers :default)
+        (:default handlers)
+        (fn [request])))
+    )))
+
+(let [multicontent-user {:default (fn[{{id :id}:params}] (decorate (lookup-user id)))
+                         "text/vcard"  (fn[{{id :id}:params}] (decorate-vcard (transform-vcard (lookup-user id)))))} ]
 
 (defroutes app-routes
   (GET "/" [] (decorate (basement.interface.index/index)))
@@ -27,9 +55,12 @@
   (GET "/users/add" [] basement.interface.users/users-add)
   (POST "/users/add" [] basement.interface.users/users-add-post)
   (GET "/users/:id" [id] (decorate (lookup-user id)))
+  (GET "/userstest/:id" [id] (decorate-vcard (transform-vcard (lookup-user id))))
   (DELETE "/users/:id" [id] (basement.interface.users/delete-user id))
   (GET "/hello" [] "Good day fellow!")
+  (GET "/test/:id" [id] (negotiator multicontent-user))
 	)
+)
 
 (def basement
   (-> app-routes
