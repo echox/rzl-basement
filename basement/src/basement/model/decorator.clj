9d98282ca03489515fb483e3106937ea36a2d2ce
Simon Koelsch <echox@echox.de>
2015-03-13 22:09:20 +0100

    link to the vcard from the users

--- basement/src/basement/model/decorator.clj
+++ basement/src/basement/model/decorator.clj
@@ -10,6 +10,11 @@
             "application/org.raumzeitlabor.benutzerdb-v1+json"
              (uri-self user)))
 
+(defn add-vcard [user]
+  (json/link "self"
+            "text/vcard"
+             (uri-self user)))
+
 (defn add-delete [user]
   (json/link "delete"
              "application/org.raumzeitlabor.benutzerdb-v1+json"
@@ -23,7 +28,8 @@
   (assoc user
   :link
     [(add-delete user)
-     (add-self user)]))
+     (add-self user)
+     (add-vcard user)]))
 
 (defn decorate-user [user]
   (if-not (empty? user)
